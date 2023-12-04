c8754c34ecfb84b90bcaaceb87f9443a7d13bc3e
Renzo Payero <renzopayero@hotmail.com>
2023-12-04 12:37:21 -0300

    agregamos el form y el estilado del endpoint registro

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
+import '../stylesheets/register/register.css'
+import "../stylesheets/register/input.css"
+import "../stylesheets/register/button.css"
+import "../api/auth.js"
+import { registerRequest } from '../api/auth.js'
 
 const RegisterPage = () => {
 
   const {register, handleSubmit} = useForm()
 
   return (
-    <div>
-      <form action="">
-        <input type="text" {...register('username', {required: true})}/>
-        <input type="email" name="" id="" />
-        <input type="password" name="" id="" />
+    <div className='divRegister'>
+      <form className='formRegister' onSubmit={handleSubmit(async(data) => {
+        console.log(data);
+        const res = await registerRequest(data)
+        console.log(res);
+      })}>
+        <div>
+          <label htmlFor="username">Nombre de usuario</label>
+          <input type="text" {...register('username', {required: true})}/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" {...register("email", {required:true})} />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña</label>
+          <input type="password" {...register("password", {required: true})} />
+        </div>
+        <button type='sumbit'>
+          <span>Registrarse</span>
+        </button>
       </form>
     </div>
   )
