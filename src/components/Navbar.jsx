b884ef9a4bd804b2528d8f86844fb4e1f0940142
estefa99 <estefanobugari@gmail.com>
2023-12-11 21:53:20 -0300

    Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import "../stylesheets/navbar/navbar.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 
 
 
 const Navbar = () => {
   const { isAuthenticated, logout, user } = useAuth();
-  const element = <FontAwesomeIcon icon={faCircleUser} />
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
@@ -28,9 +30,14 @@ const Navbar = () => {
         {
           isAuthenticated ? (
             <>
-              <li><Link to="/products">Productos</Link></li>
-              <li><Link to="/add-product">Agregar producto </Link></li>
-              <li><Link to="/" onClick={() => logout()}>Cerrar sesión</Link></li>
+              {/* Estos se renderizan para desktop */}
+              <li className="navWord"> <Link to="/products">Productos</Link> </li>
+              <li className="navWord"> <Link to="/add-product">Agregar producto </Link> </li>
+              <li className="navWord"> <Link to="/" onClick={() => logout()}>Cerrar sesión</Link> </li>
+              {/* Los tres de abajo solo se renderizan si la pantalla es menor a 600px establecido en el media querie de css */}
+              <li className="navIcon"><Link to="/products"> <FontAwesomeIcon icon={faClipboard} /></Link> </li>  
+              <li className="navIcon"><Link to="/add-product"> <FontAwesomeIcon icon={faPlus} /> </Link> </li>  
+              <li className="navIcon"><Link to="/" onClick={() => logout()}> <FontAwesomeIcon icon={faCircleXmark} /></Link> </li>  
             </>
 
           ) : (
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 import "../stylesheets/title/title.css";
 
 const Title = ({ word1, word2 }) => {
-    return (
-        <div className="container">
-            <h2 className="title">
-                <span className="title-word title-word-1">{word1}</span>
-                <span className="title-word title-word-2">{word2}</span>
-            </h2>
-        </div>
-    )
-}
+  
+  return (
+    <div className="container">
+      <h2 className="title">
+        <span className="title-word title-word-1">{word1}</span>
+        <span className="title-word title-word-2">{word2}</span>
+      </h2>
+    </div>
+  );
+};
 
-export default Title
+export default Title;
