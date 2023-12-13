166f509bca2f7536ff48fbea69076f22ef203ff8
Renzo Payero <renzopayero@hotmail.com>
2023-12-13 08:02:52 -0300

    eliminando codigo comentado inutilizado

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useProducts } from "../context/ProductsContext";
-
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ProductCard = ({ product, setModal, completForm, mostrarFooter }) => {
