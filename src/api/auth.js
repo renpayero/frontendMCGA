fa424532f0e7429cc59ea709cc10a727c5c7d266
Renzo Payero <renzopayero@hotmail.com>
2023-12-17 21:49:48 -0300

    api auth modificado

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,9 +2,9 @@ import instance from './axios.js';
 
 //axios es la variable instance en axios.js
 
-export const registerRequest = user => instance.post(`/register`, user);
+export const registerRequest = user => instance.post(`/register`, user, { withCredentials: true });
 
-export const loginRequest = user => instance.post(`/login`, user);
+export const loginRequest = user => instance.post(`/login`, user, { withCredentials: true });
 
 //Esta ruta verifica si el usuario tiene token cuando se recarga la pagina o se carga
 export const verifyTokenRequest = () => instance.get(`/verify`);
--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 //Establecemos la propiedades a axios para comunicarnos con el backend accediendo a las cookies que nos manda.
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
-  withCredentials: true
-})
+  baseURL: "https://backendmcga-production.up.railway.app/api/",
+  withCredentials: true,
+});
 
 export default instance;
+
+
+// https://backendmcga-production.up.railway.app/api/
